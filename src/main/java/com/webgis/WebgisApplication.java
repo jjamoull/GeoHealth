2ae6edd7dd53ef08eb1452cd317a2c80e9de916f
jjamoull <julien.jamoulle@student.unamur.be>
2025-12-01 19:11:32 +0100

    Update WebgisApplication.java

--- src/main/java/com/webgis/WebgisApplication.java
+++ src/main/java/com/webgis/WebgisApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class WebgisApplication {
 
+	// a comment to test pipeline
 	public static void main(String[] args) {
 		SpringApplication.run(WebgisApplication.class, args);
 	}
