90eba32ee50cd57aac2ffce3dc341f9f54aa50d6
tomiboul <115427147+tomiboul@users.noreply.github.com>
2025-12-03 14:21:30 +0100

    test push main

--- src/main/java/com/webgis/WebgisApplication.java
+++ src/main/java/com/webgis/WebgisApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class WebgisApplication {
-
+    //test tomiboul
 	public static void main(String[] args) {
 		SpringApplication.run(WebgisApplication.class, args);
 	}
