344c79ad2f4b3f3e240e483379be7aa81b2794fe
Youra73 <houria.ariouat@student.unamur.be>
2026-02-07 15:46:40 +0100

    feat : Add routes for the session
    Changed file : Frontend_angular/src/app/app.routes.ts
    
    Adding routes for login, logout, register, forgotpassword and resetpassword

--- Frontend_angular/src/app/app.routes.ts
+++ Frontend_angular/src/app/app.routes.ts
@@ -1,16 +1,18 @@
 import { Routes } from '@angular/router';
 import {Home} from './components/home/home';
-import {Authentication} from './components/authentication/authentication';
 import {Map} from './components/map/map';
 import {Navigation} from './components/navigation/navigation';
+import { Login } from './components/login/login';
+import { Register } from './components/register/register';
+import { Forgotpassword } from './components/forgotpassword/forgotpassword';
+import { Resetpassword } from './components/resetpassword/resetpassword';
+import { Logout } from './components/logout/logout';
+
 
 export const routes: Routes = [
   {
     path: 'home',
     component: Home,
-  },{
-    path: 'authentification',
-    component: Authentication,
   },{
     path: 'map',
     component: Map,
@@ -22,6 +24,21 @@ export const routes: Routes = [
   path: '',
   redirectTo: 'home',
   pathMatch: 'full'
+  },{
+    path:'login',
+    component: Login,
+  },{
+    path:'register',
+    component: Register,
+  },{
+    path:'forgot-password',
+    component: Forgotpassword,
+  },{
+    path:'reset-password',
+    component: Resetpassword,
+  },{
+    path:'logout',
+    component: Logout,
   },{
     path: '**',
     redirectTo: 'home',
