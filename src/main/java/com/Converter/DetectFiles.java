9144781107d3b0e8d41560545b87c699e3fc8cdf
tomiboul <115427147+tomiboul@users.noreply.github.com>
2026-02-21 11:30:36 +0100

    feat : add structure of transformation of Files and methods  to detect .shp file
    Changed files :
    - src/main/java/com/Converter/DetectFiles.java
    - src/main/java/com/Converter/ShapeFileToGeoJsonFile.java
    
    Move the transformation file into a new package adapted
    Add methods in java file "DetectFiles.java" to spot .shp file

---
+++ src/main/java/com/Converter/DetectFiles.java
@@ -0,0 +1,54 @@
+package com.Converter;
+
+
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+
+/**
+ * @overview This class allow to detect a type of file ".cpg, .dbf, .prj, .shp, .shx" which be used
+ * by method "transformShapeFileToGeoJsonFile(…)" in public Class : Converter/ShapeFileToGeoJsonFile
+ * */
+public class DetectFiles {
+
+    static Logger logger = Logger.getLogger(DetectFiles.class.getName());
+
+    
+    /**
+     * Find the shp file among the set of files receive in params
+     *
+     * @param fileInParam : set of file to analyze
+     * @return a shp file : if a shp file is detected among the set of files
+     *         null : otherwise
+     * */
+    public static File findShpFile(File fileInParam ) throws IOException{
+        for (File file:fileInParam.listFiles()){
+            byte[] byteOfFile = Files.readAllBytes(file.toPath());
+            if (detectShpFile(byteOfFile) == true){
+                logger.setLevel(Level.INFO);
+                logger.info("A .shp file is detected and returned");
+                return file;
+            }
+        }
+        return null;
+    }
+
+    /**
+     * Detect if the file in byte array is a shp file or not
+     *
+     * @param byteOfFile : a byte array of the file we search to detect if it is a .shp file
+     * @return true  : if it is a .shp file
+     *         false : otherwise
+     * */
+    private static boolean detectShpFile(byte[] byteOfFile) {
+        return byteOfFile[0] == (byte) 0x00 &&
+                byteOfFile[1] == (byte) 0x00 &&
+                byteOfFile[2] == (byte) 0x27 &&
+                byteOfFile[3] == (byte) 0x0A;
+    }
+
+}
---
+++ src/main/java/com/Converter/ShapeFileToGeoJsonFile.java
@@ -0,0 +1,20 @@
+package com.Converter;
+
+
+
+/**
+ * @overview This class allow thanks to the method transformShapeFileToGeoJsonFile(…) to transform
+ * a shapefile consisting of files ".cpg, .dbf, .prj, .shp, .shx" to be transformed into a GeoJson file.
+ * The GeoJson file will be used to display data on the map
+ * */
+public class ShapeFileToGeoJsonFile {
+
+
+
+    /**
+     *
+     * */
+    public static void transformShapeFileToGeoJsonFile(){
+
+    }
+}
