95ece7a6976a83ae5b158456990b83e5a977f6db
jjamoull <julien.jamoulle@student.unamur.be>
2026-02-27 17:33:37 +0100

    fix: hiding sensible information
    file changed:
    
    backend\src\main\java\com\webgis\admin\AdminInitializer.java
    
    backend\src\main\resources\application.properties
    
    hiding admin password information into the env

--- backend/src/main/java/com/webgis/admin/AdminInitializer.java
+++ backend/src/main/java/com/webgis/admin/AdminInitializer.java
@@ -1,12 +1,16 @@
 package com.webgis.admin;
 
 import com.webgis.user.UserService;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class AdminInitializer {
+
+    @Value("${superadmin.secret}")
+    private String adminPassword;
 
     @Bean
     CommandLineRunner initAdmin(UserService userService) {
@@ -17,7 +21,7 @@ public class AdminInitializer {
                         "Admin",
                         "User",
                         "admin@webgis.com",
-                        "admin123",
+                        adminPassword,
                         "SUPERADMIN"
                 );
                 System.out.println("Admin user created!");
