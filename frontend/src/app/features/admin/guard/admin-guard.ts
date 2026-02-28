7747008f438828bfcd6fa5a5b797e3b41bd855ee
jjamoull <julien.jamoulle@student.unamur.be>
2026-02-28 15:47:20 +0100

    fix: adding a spec to admin guard
    file changed:
    
    frontend\src\app\features\admin\guard\admin-guard.ts
    
    add a comment to tell the purpose of the admin guard

--- frontend/src/app/features/admin/guard/admin-guard.ts
+++ frontend/src/app/features/admin/guard/admin-guard.ts
@@ -3,6 +3,10 @@ import {UsersServices} from '../../../core/service/UserService/users-services';
 import {inject} from '@angular/core';
 import {catchError, map, of} from 'rxjs';
 
+/**
+ * Guard that check if a user is aan admin or not.
+ * If admin allow to go to the route you want, otherwise redirect to home.
+ */
 export const adminGuard: CanActivateFn = (route, state) => {
 
   const usersService= inject(UsersServices);
