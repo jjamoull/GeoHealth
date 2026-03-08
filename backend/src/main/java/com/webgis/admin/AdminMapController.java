9eddecedbabb57bd0b56896adf0bf4c3bc6c8aea
jjamoull <julien.jamoulle@student.unamur.be>
2026-03-08 15:16:43 +0100

    fix: checkstyle errors

--- backend/src/main/java/com/webgis/admin/AdminMapController.java
+++ backend/src/main/java/com/webgis/admin/AdminMapController.java
@@ -5,7 +5,12 @@ import com.webgis.map.Map;
 import com.webgis.map.MapService;
 import com.webgis.map.dto.MapListDto;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
