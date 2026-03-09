c447334956fb133b2596addc9c3bd18b03b0c5c1
Youra73 <houria.ariouat@student.unamur.be>
2026-03-09 14:44:07 +0100

    feat: Adding shared component sidebar
    changed file :
    -frontend\src\app\shared\components\unconnectedsidebar\unconnectedsidebar.css
    -frontend\src\app\shared\components\unconnectedsidebar\unconnectedsidebar.html
    -frontend\src\app\shared\components\unconnectedsidebar\unconnectedsidebar.spec.ts
    -frontend\src\app\shared\components\unconnectedsidebar\unconnectedsidebar.ts
    
    Adding the component for the unconnected sidebar (for phone)

---
+++ frontend/src/app/shared/components/unconnectedsidebar/unconnectedsidebar.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-unconnectedsidebar',
+  imports: [],
+  templateUrl: './unconnectedsidebar.html',
+  styleUrl: './unconnectedsidebar.css',
+})
+export class Unconnectedsidebar {
+
+}
