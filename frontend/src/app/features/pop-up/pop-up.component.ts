2a5e90c8238927d0745867f60bc62c1e60f08441
tomiboul <115427147+tomiboul@users.noreply.github.com>
2026-03-10 15:03:30 +0100

    fix : refactor of frontend on PopUp Componant
    Changed files :
    frontend/src/app/features/pop-up/final-map/final-map.css
    frontend/src/app/features/pop-up/final-map/final-map.html
    frontend/src/app/features/pop-up/final-map/final-map.spec.ts
    frontend/src/app/features/pop-up/final-map/final-map.ts
    frontend/src/app/features/pop-up/pop-up.component.html
    frontend/src/app/features/pop-up/pop-up.component.ts
    frontend/src/app/features/pop-up/risk-factor/risk-factor.css
    frontend/src/app/features/pop-up/risk-factor/risk-factor.html
    frontend/src/app/features/pop-up/risk-factor/risk-factor.spec.ts
    frontend/src/app/features/pop-up/risk-factor/risk-factor.ts
    frontend/src/app/shared/components/button.component/button.component.css
    .gitignore
    
    Split the HTML code for the popup component into two separate components to improve readability.
    Update of shared component Button to have a better display

--- frontend/src/app/features/pop-up/pop-up.component.ts
+++ frontend/src/app/features/pop-up/pop-up.component.ts
@@ -3,136 +3,38 @@ import {MatDialogRef, MAT_DIALOG_DATA} from '@angular/material/dialog';
 import {FormControl, FormGroup, ReactiveFormsModule, Validators} from '@angular/forms';
 import {FinalMapService} from '../../core/service/MapService/FinalMapService/finalMapService';
 import {RiskFactorMapService} from '../../core/service/MapService/RiskMapService/riskFactorMapService';
+import {FinalMap} from './final-map/final-map';
+import {RiskFactor} from './risk-factor/risk-factor';
 
 @Component({
   selector: 'app-pop-up',
   imports: [
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    FinalMap,
+    RiskFactor
   ],
   templateUrl: './pop-up.component.html',
   styleUrl: './pop-up.component.css',
 })
-export class PopUpComponent implements OnInit{
-  constructor(private dialog: MatDialogRef <PopUpComponent>,
-              private finalMapService: FinalMapService,
-              private riskFactorMapService: RiskFactorMapService,
-              @Inject(MAT_DIALOG_DATA) public data: any) {}
-
+export class PopUpComponent implements OnInit {
+  constructor(@Inject(MAT_DIALOG_DATA) public data: { typeOfPopUp: string }) {}
   //************ Constants ************
   addMap:string ="addMap";
   addRiskFactor : string = "addRiskFactor"
   //***********************************
 
   //******** Global variables *********
-  problemWithUploading:boolean = false;
-  typeOfPopUp: string = "addRiskFactor";
-  formGroup!: FormGroup;
-  selectedFile: File | null = null;
-  isUploading = false;
-  @Input()
-  requiredFileType:string = '';
+  typeOfPopUp: string = "null";
   //***********************************
-
-
-
-  /**
-   * Init the form to add the new map on the list of map
-   */
   ngOnInit():void {
-    this.problemWithUploading = false;
-
     this.typeOfPopUp = this.data.typeOfPopUp;
-    this.formGroup = new FormGroup({
-      title: new FormControl('',[Validators.required, Validators.minLength(1)]),
-      description: new FormControl('', [Validators.required, Validators.minLength(1)]),
-      file: new FormControl(null, [Validators.required])
-    });
   }
 
 
 
   /**
-   * Handles the file upload component contained in a pop-up
+   * Check the type of popup component that will be called and display it
    * */
-  onFileSelected(event:Event) {
-    this.problemWithUploading = false;
-
-    // This constant recieve the set of data from the event
-    const input = event.target as HTMLInputElement;
-
-    if (input.files && input.files.length > 0) {
-      this.selectedFile = input.files[0];
-      this.isUploading = false;
-    }
-  }
-
-  /**
-   * Methods that contain all situation of sending data to backend
-   * */
-  private sendData(formData: FormData){
-    console.log("typeOfPopOp =", this.typeOfPopUp);
-
-    if (this.typeOfPopUp == this.addMap){
-
-      this.finalMapService.uploadNewMap(formData).subscribe(
-        {
-          next:()=>{
-            this.isUploading = false;
-            //this.closePopUp()
-          }, error:(error)=>{
-            console.error(error);
-            this.isUploading = false;
-          }
-        }
-      );
-    } else if(this.typeOfPopUp == this.addRiskFactor) {
-      this.riskFactorMapService.uploadNewRiskFactor(formData).subscribe(
-        {
-          next:()=>{
-            this.isUploading = false;
-            this.dialog.close();
-
-            //this.closePopUp()
-          }, error:(error)=>{
-            this.problemWithUploading = true;
-            console.error(error);
-            this.isUploading = false;
-          }
-        }
-      );
-    } else {
-      throw new Error("Impossible to send data to add your new feature ! \n Issue with the type of feature you want to add");
-    }
-  }
-
-  /**
-   * Allow the user to close the pop-up
-   * */
-  closePopUp(): void {
-    if (!this.selectedFile) {
-      return;
-    }
-
-    //add all values from the form in FormData to send in DB
-    const formData = new FormData();
-    formData.append("title", this.formGroup.value.title);
-    formData.append("description", this.formGroup.value.description);
-    formData.append("tifFile", this.selectedFile);
-
-    this.sendData(formData);
-  }
-
-
-  /**
-   * Allow to show at the user if his input is correct or not before to send it to the backend
-   * Check precondition of the type of transformation
-   * */
-  isFieldValid(name: string){
-    const formControl = this.formGroup.get(name);
-    return formControl?.invalid && formControl?.dirty;
-  }
-
-
   typeOfPopopIsValid(name:string){
     if (this.typeOfPopUp == name){
       return true;
---
+++ frontend/src/app/features/pop-up/risk-factor/risk-factor.ts
@@ -0,0 +1,136 @@
+import {Component, EventEmitter, Inject, Input, OnInit, Output} from '@angular/core';
+import {FormControl, FormGroup, ReactiveFormsModule, Validators} from '@angular/forms';
+import {MAT_DIALOG_DATA, MatDialogRef} from '@angular/material/dialog';
+import {FinalMapService} from '../../../core/service/MapService/FinalMapService/finalMapService';
+import {RiskFactorMapService} from '../../../core/service/MapService/RiskMapService/riskFactorMapService';
+
+@Component({
+  selector: 'app-risk-factor',
+  imports: [
+    ReactiveFormsModule
+  ],
+  templateUrl: './risk-factor.html',
+  styleUrl: './risk-factor.css',
+})
+export class RiskFactor  implements OnInit{
+  constructor(private dialog: MatDialogRef <RiskFactor>,
+              private finalMapService: FinalMapService,
+              private riskFactorMapService: RiskFactorMapService,
+              @Inject(MAT_DIALOG_DATA) public data: any) {}
+
+  //************ Constants ************
+  addMap:string ="addMap";
+  addRiskFactor : string = "addRiskFactor"
+  //***********************************
+
+  //******** Global variables *********
+  problemWithUploading:boolean = false;
+  typeOfPopUp: string = "addRiskFactor";
+  formGroup!: FormGroup;
+  selectedFile: File | null = null;
+  isUploading = false;
+  @Input()
+  requiredFileType:string = '';
+  //***********************************
+
+
+
+  /**
+   * Init the form to add the new map on the list of map
+   */
+  ngOnInit():void {
+    this.problemWithUploading = false;
+
+    this.typeOfPopUp = this.data.typeOfPopUp;
+    this.formGroup = new FormGroup({
+      title: new FormControl('',[Validators.required, Validators.minLength(1)]),
+      description: new FormControl('', [Validators.required, Validators.minLength(1)]),
+      file: new FormControl(null, [Validators.required])
+    });
+  }
+
+
+
+  /**
+   * Handles the file upload component contained in a pop-up
+   * */
+  onFileSelected(event:Event) {
+    this.problemWithUploading = false;
+
+    // This constant recieve the set of data from the event
+    const input = event.target as HTMLInputElement;
+
+    if (input.files && input.files.length > 0) {
+      this.selectedFile = input.files[0];
+      this.isUploading = false;
+    }
+  }
+
+  /**
+   * Methods that contain all situation of sending data to backend
+   * */
+  private sendData(formData: FormData){
+    console.log("typeOfPopOp =", this.typeOfPopUp);
+
+    if (this.typeOfPopUp == this.addMap){
+
+      this.finalMapService.uploadNewMap(formData).subscribe(
+        {
+          next:()=>{
+            this.isUploading = false;
+            //this.closePopUp()
+          }, error:(error)=>{
+            console.error(error);
+            this.isUploading = false;
+          }
+        }
+      );
+    } else if(this.typeOfPopUp == this.addRiskFactor) {
+      this.riskFactorMapService.uploadNewRiskFactor(formData).subscribe(
+        {
+          next:()=>{
+            this.isUploading = false;
+            this.dialog.close();
+
+            //this.closePopUp()
+          }, error:(error)=>{
+            this.problemWithUploading = true;
+            console.error(error);
+            this.isUploading = false;
+          }
+        }
+      );
+    } else {
+      throw new Error("Impossible to send data to add your new feature ! \n Issue with the type of feature you want to add");
+    }
+  }
+
+  /**
+   * Allow the user to close the pop-up
+   * */
+  closePopUp(): void {
+    if (!this.selectedFile) {
+      return;
+    }
+
+    //add all values from the form in FormData to send in DB
+    const formData = new FormData();
+    formData.append("title", this.formGroup.value.title);
+    formData.append("description", this.formGroup.value.description);
+    formData.append("tifFile", this.selectedFile);
+
+    this.sendData(formData);
+  }
+
+
+  /**
+   * Allow to show at the user if his input is correct or not before to send it to the backend
+   * Check precondition of the type of transformation
+   * */
+  isFieldValid(name: string){
+    const formControl = this.formGroup.get(name);
+    return formControl?.invalid && formControl?.dirty;
+  }
+
+
+}
