d4346448bdddeb7a876694988c02fdaf71cce3db
Venator‬‏ <anas.damlakhi@student.unamur.be>
2026-03-14 17:51:47 +0100

    fix: checkstyle

--- backend/src/main/java/com/webgis/map/riskmap/tile/TileRepository.java
+++ backend/src/main/java/com/webgis/map/riskmap/tile/TileRepository.java
@@ -2,7 +2,5 @@ package com.webgis.map.riskmap.tile;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.Optional;
-
 public interface TileRepository extends JpaRepository<Tile, TileId> {
 }
