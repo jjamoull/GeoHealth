5b1047b90fa3943092729178abaa02914e934f67
adamlakh <mdamlakhi73@gmail.com>
2026-03-18 10:41:00 +0100

    feat: evaluation modal design
    done html + css for the evaluation modal

--- frontend/src/app/features/map/evaluation-modal/evaluation-modal.ts
+++ frontend/src/app/features/map/evaluation-modal/evaluation-modal.ts
@@ -1,11 +1,12 @@
 import { Component, Input, Output, EventEmitter } from '@angular/core';
 import { CommonModule} from '@angular/common';
 import { CdkDrag, CdkDragHandle } from '@angular/cdk/drag-drop';
+import { ButtonComponent } from '../../../shared/components/button.component/button.component';
 
 @Component({
   selector: 'app-evaluation-modal',
   standalone: true,
-  imports: [CommonModule, CdkDrag, CdkDragHandle],
+  imports: [CommonModule, CdkDrag, CdkDragHandle, ButtonComponent],
   templateUrl: './evaluation-modal.html',
   styleUrl: './evaluation-modal.css',
 })
