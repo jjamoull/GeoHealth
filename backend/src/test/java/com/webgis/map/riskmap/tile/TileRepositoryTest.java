601d7048d4e4a34ea74052302970caca8ac23b4f
LouisBomal <louis.bomal@student.unamur.be>
2026-03-30 17:47:27 +0200

    test : adding tests for maps services/repositories/controllers
    backend\src\main\java\com\webgis\map\riskmap\tile\TileId.java
    backend\src\main\java\com\webgis\map\riskmap\tile\TileRepository.java
    backend\src\test\java\com\webgis\map\finalMap\finalMapControllerTest.java
    backend\src\test\java\com\webgis\map\finalMap\FinalMapEntityTest.java
    backend\src\test\java\com\webgis\map\finalMap\FinalMapRespositoryTest.java
    backend\src\test\java\com\webgis\map\finalMap\FinalMapServiceTest.java
    backend\src\test\java\com\webgis\map\riskmap\riskfactormap\RiskFactorMapRepositoryTest.java
    backend\src\test\java\com\webgis\map\riskmap\riskfactormap\RiskFactorMapServiceTest.java
    backend\src\test\java\com\webgis\map\riskmap\tile\TileRepositoryTest.java
    backend\src\test\java\com\webgis\map\riskmap\tile\TileServiceTest.java
    backend\src\test\java\com\webgis\riskFactorMap\riskFactorMapTest.java
    
    adding tests for final maps and risk factor maps (services/controllers/JPArepositories)
    
    Co-Authored-By: Thomas Buse au nid <115427147+tomiboul@users.noreply.github.com>

---
+++ backend/src/test/java/com/webgis/map/riskmap/tile/TileRepositoryTest.java
@@ -0,0 +1,89 @@
+package com.webgis.map.riskmap.tile;
+
+
+import com.webgis.map.riskmap.riskfactormap.RiskFactorMap;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+
+import java.util.Optional;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+@DataJpaTest
+public class TileRepositoryTest {
+    @Autowired
+    private TileRepository tileRepository;
+
+    private void assertMapEquals(Tile actual, Tile expected) {
+        assertThat(actual.getTileId()).isEqualTo(expected.getTileId());
+        assertThat(actual.getTileData()).containsExactly(expected.getTileData());    }
+
+    private TileId tileId2;
+    private TileId tileId3;
+    private Tile tile1;
+    private Tile tile2;
+
+
+    @BeforeEach
+    void init(){
+        // Arrange
+        RiskFactorMap riskFactorMap = new RiskFactorMap("Title1", "Description1");
+        RiskFactorMap riskFactorMap2 = new RiskFactorMap("Title2", "Description2");
+        TileId tileId1 = new TileId(1L,1,1,1);
+        tileId2 = new TileId(2L,2,2,2);
+        tileId3 = new TileId(3L,3,3,3);
+        byte[] byteArray1 = {62};
+        byte[] byteArray2 = {32};
+
+        tile1 = new Tile(tileId1, byteArray1, riskFactorMap);
+        tile2 = new Tile(tileId2, byteArray2, riskFactorMap2);
+        tile3 = new Tile(tileId3, byteArray2, riskFactorMap2);
+    }
+
+
+    @Test
+    void findByIdIsOKTest(){
+
+
+        // Act
+        tile1 = tileRepository.save(tile1);
+        tile2 = tileRepository.save(tile2);
+
+        Optional<Tile> result1 = tileRepository.findByTileId(tile1.getTileId());
+        Optional<Tile> result2 = tileRepository.findByTileId(tile2.getTileId());
+
+        // Assert
+        assertNotNull(result1);
+        assertNotNull(result2);
+
+        assertTrue(result1.isPresent());
+        assertTrue(result2.isPresent());
+
+        assertMapEquals(tile1, result1.get());
+        assertMapEquals(tile2, result2.get());
+    }
+
+
+    @Test
+    void notFindByIdIsOKTest(){
+
+        // Act
+        tile1 = tileRepository.save(tile1);
+
+        Optional<Tile> result1 = tileRepository.findByTileId(tile1.getTileId());
+        Optional<Tile> result2 = tileRepository.findByTileId(tileId3);
+
+
+        // Assert
+        assertNotNull(result1);
+
+        assertTrue(result2.isEmpty());
+
+        assertMapEquals(tile1, result1.get());
+    }
+}
---
+++ backend/src/test/java/com/webgis/map/riskmap/tile/TileServiceTest.java
@@ -0,0 +1,60 @@
+package com.webgis.map.riskmap.tile;
+
+
+import com.webgis.map.riskmap.riskfactormap.RiskFactorMap;
+import com.webgis.map.riskmap.riskfactormap.RiskFactorMapRepository;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+
+import java.util.Optional;
+
+import static org.hibernate.validator.internal.util.Contracts.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.*;
+
+@ExtendWith(MockitoExtension.class)
+class TileServiceTest {
+
+    @Mock
+    private TileRepository tileRepository;
+
+    @Mock
+    private RiskFactorMapRepository riskFactorMapRepository;
+
+    @InjectMocks
+    private TileService tileService;
+
+
+    private long mapId = 1L;
+    private int zoom = 2;
+    private int x = 3;
+    private int y = 4;
+    private final byte[] data = new byte[]{1,2,3};
+    private final RiskFactorMap riskFactorMap = new RiskFactorMap("Title1", "Description1");
+
+    private final TileId expectedId = new TileId(mapId, zoom, x, y);
+    private final Tile expectedTile = new Tile(expectedId, data, riskFactorMap);
+    private final RiskFactorMap expectedRiskFactorMap = new RiskFactorMap("Title2", "Description2");
+
+    @Test
+    void testSaveTile() {
+        tileService = new TileService(tileRepository, riskFactorMapRepository);
+        when(tileRepository.save(any(Tile.class))).thenReturn(expectedTile);
+        when(riskFactorMapRepository.findById(mapId)).thenReturn(Optional.of(expectedRiskFactorMap));
+
+        Tile result = tileService.save(mapId, zoom, x, y, data);
+
+        assertNotNull(result);
+        assertEquals(mapId, result.getTileId().getMapId());
+        assertEquals(zoom, result.getTileId().getZoom());
+        assertEquals(x, result.getTileId().getX());
+        assertEquals(y, result.getTileId().getY());
+
+        verify(tileRepository, times(1)).save(any(Tile.class));
+    }
+}
