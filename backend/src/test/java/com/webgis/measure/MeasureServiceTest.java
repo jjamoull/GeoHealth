59790ad5e4186138e40ea4d75ac4be54bd77474c
‪ANAS DAMLAKHI‬‏ <119134304+adamlakh@users.noreply.github.com>
2026-03-30 15:58:26 +0200

    Change MeasureServiceTest class visibility to package-private

--- backend/src/test/java/com/webgis/measure/MeasureServiceTest.java
+++ backend/src/test/java/com/webgis/measure/MeasureServiceTest.java
@@ -17,7 +17,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
-public class MeasureServiceTest {
+class MeasureServiceTest {
 
     @Mock
     private EvaluationFormRepository evaluationFormRepository;
