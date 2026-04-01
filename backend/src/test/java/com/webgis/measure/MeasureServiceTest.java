2c0bb90bdd020a6e2c061fd47b5f8a1a12ad0c5c
jjamoull <julien.jamoulle@student.unamur.be>
2026-04-01 14:36:16 +0200

    fix: removing useless import after sonar analysis
    file changed : backend\src\test\java\com\webgis\measure\MeasureServiceTest.java

--- backend/src/test/java/com/webgis/measure/MeasureServiceTest.java
+++ backend/src/test/java/com/webgis/measure/MeasureServiceTest.java
@@ -11,7 +11,6 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
-import java.io.IOException;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
