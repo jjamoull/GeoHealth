ddacde1974966274b40568c221ffd1d658296a09
jjamoull <julien.jamoulle@student.unamur.be>
2026-04-01 14:31:10 +0200

    fix: removing import for checkstyle
    file changed: backend\src\main\java\com\webgis\measure\MeasureController.java

--- backend/src/main/java/com/webgis/measure/MeasureController.java
+++ backend/src/main/java/com/webgis/measure/MeasureController.java
@@ -12,7 +12,6 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.PostMapping;
 
-import java.io.IOException;
 import java.util.Optional;
 
 @RestController
