3f22b58d940855db1d2395c9e563fd79e774356a
tomiboul <115427147+tomiboul@users.noreply.github.com>
2026-04-08 10:26:14 +0200

    fix : delete old folder
    Co-Authored-By: LouisBomal <115359454+LouisBomal@users.noreply.github.com>

--- backend/src/main/java/com/webgis/map/finalrastermap/FinalRasterMapRepository.java
+++
@@ -1,9 +0,0 @@
-package com.webgis.map.finalrastermap;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import java.util.Optional;
-
-public interface FinalRasterMapRepository extends JpaRepository<FinalRasterMap, Integer> {
-    Optional<FinalRasterMap> findByTitle(String title);
-    Optional<FinalRasterMap> findById(long id);
-}
--- backend/src/main/java/com/webgis/map/finalrastermap/dto/FinalRasterMapDto.java
+++
@@ -1,6 +0,0 @@
-package com.webgis.map.finalrastermap.dto;
-
-/**
- * DTO for final raster map list
- **/
-public record FinalRasterMapDto(Long id, String title){}
