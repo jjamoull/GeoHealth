06c059a4cfdc6a1e0a8978b2749f0e6ffe792674
Thomas Buse au nid <115427147+tomiboul@users.noreply.github.com>
2026-04-15 11:23:28 +0200

    Update zoom range in transformIntoXYZFile

--- backend/src/main/java/com/converter/TiffFiles.java
+++ backend/src/main/java/com/converter/TiffFiles.java
@@ -59,7 +59,7 @@ public class TiffFiles {
                     "gdal2tiles.py", // the command
                     "--xyz", // format to be used by leaflet on OSM
                     "-z",
-                    "0-7", // represent zoom from 0 to 12 on Z (<-> "-z")
+                    "0-12", // represent zoom from 0 to 12 on Z (<-> "-z")
                     temp8bit.toAbsolutePath().toString(), // file in 8 bit from
                     outputDir.toAbsolutePath().toString() // Destination of the file
             };
