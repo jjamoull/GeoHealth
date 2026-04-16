57aa7f21a6f7db6de4a5c20ba908cf6ac5bcfbb2
tomiboul <115427147+tomiboul@users.noreply.github.com>
2026-04-16 21:32:33 +0200

    feat : add DTO and buisness logic with service for annotations
    Changed files :
    frontend/src/app/core/service/MapService/AnnotationService/AnnotationService.ts
    frontend/src/app/shared/models/MapModel/AnnotationModel/AnnotationDTO.ts
    backend/src/main/java/com/webgis/annotations/dto/AnnotationPostDTO.java
    
    Add DTO for annotations
    Add service for request POST and GET for annotations
    
    Co-Authored-By: LouisBomal <115359454+LouisBomal@users.noreply.github.com>

--- backend/src/main/java/com/webgis/annotations/dto/AnnotationPostDTO.java
+++ backend/src/main/java/com/webgis/annotations/dto/AnnotationPostDTO.java
@@ -3,5 +3,4 @@ package com.webgis.annotations.dto;
 /**
  * Data Transfer Object containing fields for sending annotation to backend for saving purpose
  */
-public record AnnotationPostDTO(Long mapId, Long userId, String division, String geoJson) {
-}
+public record AnnotationPostDTO(Long mapId, Long userId, String division, String geoJson) {}
