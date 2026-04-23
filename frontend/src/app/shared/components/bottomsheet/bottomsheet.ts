97a9adf5ca9ba3ad84e9b44d9e04e988f5216922
Youra73 <houria.ariouat@student.unamur.be>
2026-04-23 17:12:06 +0200

    feat: Add bottom sheet shared component
    changed files:
    -frontend\src\app\shared\components\bottomsheet\bottomsheet.css
    -frontend\src\app\shared\components\bottomsheet\bottomsheet.html
    -frontend\src\app\shared\components\bottomsheet\bottomsheet.spec.ts
    -frontend\src\app\shared\components\bottomsheet\bottomsheet.ts
    
    Add bottom sheet shared component

---
+++ frontend/src/app/shared/components/bottomsheet/bottomsheet.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-bottomsheet',
+  imports: [],
+  templateUrl: './bottomsheet.html',
+  styleUrl: './bottomsheet.css',
+})
+export class Bottomsheet {
+
+}
