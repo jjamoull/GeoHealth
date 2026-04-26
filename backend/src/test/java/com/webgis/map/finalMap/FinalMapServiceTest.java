56e816b52bee5216aa1da1ecb8b6f26b3d0cdaf4
jjamoull <julien.jamoulle@student.unamur.be>
2026-04-26 15:44:59 +0200

    feat Add report tests; admin button
    file changed: see commit
    
    Adding test for controller and service for report + hiding create report btn for non admin user

--- backend/src/test/java/com/webgis/map/finalMap/FinalMapServiceTest.java
+++ backend/src/test/java/com/webgis/map/finalMap/FinalMapServiceTest.java
@@ -22,7 +22,7 @@ import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
-class finalFinalMapServiceTest {
+class FinalMapServiceTest {
 
     @Mock
     FinalMapRepository finalMapRepository;
