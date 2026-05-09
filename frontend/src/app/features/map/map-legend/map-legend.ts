a39cb4b99975affb9d40db9f775e0ff367c9a3e9
Venator‬‏ <anas.damlakhi@student.unamur.be>
2026-05-09 17:20:13 +0200

    fix: gradient work for dry and wet ebola maps

--- frontend/src/app/features/map/map-legend/map-legend.ts
+++ frontend/src/app/features/map/map-legend/map-legend.ts
@@ -10,6 +10,11 @@ import { CommonModule } from '@angular/common';
 })
 export class MapLegendComponent {
   @Input() mapType: string | undefined = '';
+  @Input() season: string | undefined = '';
+
+  get isDry(): boolean {
+    return this.season === 'DRY';
+  }
 
   get isEbola(): boolean {
     return this.mapType === 'EBOLA';
